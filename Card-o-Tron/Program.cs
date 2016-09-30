faf36db1c7c1e2b4665e1a30e6310f9e6478d7fa
SnakeTheRipper <urimr26cat@gmail.com>
2016-09-30 23:48:36 +0200

    Adding new role system and adding support for weapon cards

--- Card-o-Tron/Program.cs
+++ Card-o-Tron/Program.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.IO;
+using System;
 using System.Net;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Collections.Generic;
 using System.Net.NetworkInformation;
 using Newtonsoft.Json;
 using Discord;
@@ -31,7 +29,10 @@ namespace Card_o_Tron
 
         private string AppDirectory;
 
-        private List<string> Admins = new List<string>();
+        private Role DeveloperRole;
+        private Role AdministratorRole;
+        private Role ModeratorRole;
+        private Role VeteranRole;
 
         private WebHeaderCollection MashapeHeader = new WebHeaderCollection();
 
@@ -40,8 +41,6 @@ namespace Card_o_Tron
         public void Start()
         {
             AppDirectory = AppDomain.CurrentDomain.BaseDirectory;
-
-            LoadFiles();
 
             MashapeHeader.Add("X-Mashape-Key", "kZYPGbYm4SmshzBsE9Ftb3ON5fump1IUQhFjsnrRl51bOGh5tv");
 
@@ -62,37 +61,20 @@ namespace Card_o_Tron
 
                 Server = Client.Servers.First(s => s.Id == ServerID);
 
+                DeveloperRole = Server.FindRoles("Developers").FirstOrDefault();
+                AdministratorRole = Server.FindRoles("Administrators").FirstOrDefault();
+                ModeratorRole = Server.FindRoles("Moderators").FirstOrDefault();
+                VeteranRole = Server.FindRoles("Veterans").FirstOrDefault();
+
                 LogText("Loaded Card-o-Tron bot to server " + Server.Name);
             });
         }
 
-        private void LoadFiles()
-        {
-            if (File.Exists(AppDirectory + "admins.list"))
-            {
-                string[] admins = File.ReadAllText(AppDirectory + "admins.list").Split(new string[1] { ";" }, StringSplitOptions.RemoveEmptyEntries);
-
-                foreach (string admin in admins)
-                {
-                    Admins.Add(admin);
-                }
-
-                LogText("Loaded " + admins.Length + " admins");
-            }
-            else
-            {
-                File.Create(AppDirectory + "admins.list").Close();
-
-                LogText("Created empty admin list");
-            }
-
-            LogText(" ");
-        }
-
         private void ProcessMessage(MessageEventArgs args)
         {
-            string fullUser = args.User.ToString();
             Channel channel = args.Channel;
+            User user = args.User;
+            string fullUser = user.ToString();
 
             if (args.Message.IsAuthor == false)
             {
@@ -103,7 +85,10 @@ namespace Card_o_Tron
                     if (fullText.StartsWith("!"))
                     {
                         string[] commands = fullText.Split();
-                        bool isAdmin = Admins.Contains(fullUser);
+                        bool isDeveloper = user.HasRole(DeveloperRole);
+                        bool isAdmin = isDeveloper || user.HasRole(AdministratorRole);
+                        bool isModerator = isDeveloper || isAdmin || user.HasRole(ModeratorRole);
+                        bool isVeteran = isDeveloper || isAdmin || isModerator || user.HasRole(VeteranRole);
 
                         switch (commands[0].ToLower())
                         {
@@ -133,11 +118,6 @@ namespace Card_o_Tron
                                                         "!ping - Checks bot status\n" +
                                                         "!help - Shows this message```\n" +
 
-                                                        "**· Admin Commands: **\n" +
-                                                        "```!addadmin <fullname> - Adds an admin to the admin list (admin only)\n" +
-                                                        "!removeadmin <fullname> - Removes an admin from the admin list (admin only)\n" +
-                                                        "!adminlist - Shows the full list of admins```\n" +
-
                                                         "**· Card Commands: **\n" +
                                                         "```!card <fullname> - Sends the card as an image\n" +
                                                         "!cardgif <fullname> - Sends the card as a gif```\n" +
@@ -146,26 +126,6 @@ namespace Card_o_Tron
                                                         "```!hero <fullname> - Sends the hero as an image\n" +
                                                         "!herogif <fullname> - Sends the hero as a gif```\n");
                                 }
-                                break;
-                            case "!addadmin":
-                                if (commands.Length > 1 && isAdmin)
-                                {
-                                    LogAdminCommand(channel, commands[0], fullUser);
-                                    AddAdminCommand(channel, commands[1]);
-                                }
-                                break;
-
-                            case "!removeadmin":
-                                if (commands.Length > 1 && isAdmin)
-                                {
-                                    LogAdminCommand(channel, commands[0], fullUser);
-                                    RemoveAdminCommand(channel, commands[1]);
-                                }
-                                break;
-
-                            case "!adminlist":
-                                LogNormalCommand(channel, commands[0], fullUser);
-                                ShowAdminListCommand(channel);
                                 break;
 
                             case "!card":
@@ -206,87 +166,7 @@ namespace Card_o_Tron
         }
 
         #endregion
-
-        #region Admin Related Methods
-
-        private void ShowAdminListCommand(Channel channel)
-        {
-            if (Admins.Count > 0)
-            {
-                string adminList = "**Showing current admin list **(" + DateTime.Today.ToShortDateString() + ")** :**\n\n```";
-
-                for (int i = 0; i < Admins.Count; i++)
-                {
-                    adminList += "· " + Admins[i] + "\n";
-                }
-
-                channel.SendMessage(adminList + "```");
-            }
-            else
-            {
-                channel.SendMessage("**Admin list is empty.**");
-            }
-        }
-
-        private void AddAdminCommand(Channel channel, string admin)
-        {
-            if (Server.Users.Any(u => u.ToString() == admin))
-            {
-                if (Admins.Contains(admin))
-                {
-                    channel.SendMessage("@" + admin + "** is already an admin.**");
-                }
-                else
-                {
-                    AddAdmin(admin);
-                    channel.SendMessage("@" + admin + "** was added to the admin list.**");
-                }
-            }
-            else
-            {
-                channel.SendMessage(admin + "** was not found in the server.**");
-            }
-        }
-
-        private void RemoveAdminCommand(Channel channel, string admin)
-        {
-            if (Admins.Contains(admin))
-            {
-                RemoveAdmin(admin);
-                channel.SendMessage(admin + "** was removed from admins.**");
-            }
-            else
-            {
-                channel.SendMessage(admin + "** is not an admin.**");
-            }
-        }
-
-        private void AddAdmin(string admin)
-        {
-            Admins.Add(admin);
-            SaveAdminFile();
-        }
-
-        private void RemoveAdmin(string admin)
-        {
-            Admins.Remove(admin);
-            SaveAdminFile();
-        }
-
-        private void SaveAdminFile()
-        {
-            string adminString = string.Join(";", Admins.ToArray());
-
-            if (adminString.StartsWith(";"))
-            {
-                adminString = adminString.Substring(1);
-            }
-
-            File.WriteAllText(AppDirectory + "admins.list", adminString);
-        }
-
-        #endregion
-
+        
         #region Card Related Methods
 
         public async void CardCommand(Channel channel, string cardName)
@@ -304,7 +184,7 @@ namespace Card_o_Tron
 
                 foreach (dynamic cardObject in jsonObject)
                 {
-                    if (cardObject.type == "Minion" || cardObject.type == "Spell")
+                    if (cardObject.type == "Minion" || cardObject.type == "Spell" || cardObject.type == "Weapon")
                     {
                         foundMatch = true;
 
@@ -338,7 +218,7 @@ namespace Card_o_Tron
 
                 foreach (dynamic cardObject in jsonObject)
                 {
-                    if (cardObject.type == "Minion" || cardObject.type == "Spell")
+                    if (cardObject.type == "Minion" || cardObject.type == "Spell" || cardObject.type == "Weapon")
                     {
                         foundMatch = true;
 
